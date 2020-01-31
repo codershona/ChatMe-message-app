cfdb0dd2c68e368653ec1ec31100eb7fde1ce9a8
codershona <falgunimst95@gmail.com>
2020-01-31 19:51:19 +0100

    authentication system create and destroy method sessions added working perfectly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,37 @@
 class SessionsController < ApplicationController
 
 def new
+ 
+end
+
+
+def create
+
+   user = User.find_by(username: params[:session][:username])
+   if user && user.authenticate(params[:session][:password])
+   	
+   	session[:user_id] = user.id
+   	flash[:success] = "You have successfully logged in"
+   	redirect_to root_path
+   else
+   	flash.now[:error] = "There was something wrong with your log-in information"
+   	render 'new'
+   end
+end
+
+
+
+
+def destroy
+
+	session[:user_id] = nil
+	flash[:success] = "You have successfully logged out"
+	redirect_to login_path
+
 
 end
+
+
 
 
 end
